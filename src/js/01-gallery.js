d22ffa487ec14a5c0b11baa9e892eb82c53ac163
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-01-12 17:26:08 +0200

    1Task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContEl = document.querySelector('.gallery');
+galleryContEl.insertAdjacentHTML(
+  'afterbegin',
+  createGalleryItemList(galleryItems)
+);
 
+function createGalleryItemList(gallery) {
+  return gallery
+    .map(item => {
+      return `
+      <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    })
+    .join('');
+}
 console.log(galleryItems);
+galleryContEl.addEventListener('click', onGaleryImgClick);
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// console.log(gallery.defaultOptions);
+
+function onGaleryImgClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  gallery;
+  //   console.log("jmak");
+}
